1afc25e7bafd246564be673dc3643cb556a7cfcb
KrishnanshPuri <krishnanshpuri@gmail.com>
2026-06-11 23:48:16 +0530

    CORS

--- server/server.js
+++ server/server.js
@@ -10,7 +10,10 @@ const app = express();
 connectDB()
 
 app.use(cors({
-    origin: 'http://localhost:5173', // Replace with your exact React frontend URL!
+   origin: [
+    "http://localhost:5173", 
+    "https://your-actual-vercel-url.vercel.app" 
+  ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true 
 }));
